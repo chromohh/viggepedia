c39def82c9a1b85c2c2b9b9e6e32e41156aaec70
chromohh <sindre.he@hotmail.com>
2021-09-21 17:07:42 +0200

    Revert "Updates"
    This reverts commit 0721559e72d180d85be394d00e857ff20c06dbe5.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import { TiktokView } from './Views/Tiktok/TiktokView';
+
+function App() {
+  return (
+    <div className="App" >
+      <h1 className="Head1">Viggepedia!</h1>
+      <TiktokView></TiktokView>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Views/Tiktok/TiktokView.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TikTokList } from "./TikTokList";
+
+export const TiktokView = () => {
+    return (
+    <div>
+        <TikTokList/>
+    </div>
+    
+)}
